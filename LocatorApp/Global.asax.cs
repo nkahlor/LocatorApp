7e65bf9f09efd63210058b3fc047857abb5b098a
Scotty Molt <smolt@asu.edu>
2018-11-18 12:58:20 -0700

    Part A: Global asax file done

--- LocatorApp/Global.asax.cs
+++ LocatorApp/Global.asax.cs
@@ -11,11 +11,45 @@ namespace LocatorApp
 {
     public class Global : HttpApplication
     {
+        const string SESSION_COUNT = "SessionCounter";
+
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Application [ SESSION_COUNT ] = 0;
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            Response.Write("<hr />The Website was last visited on " + DateTime.Now.ToString());
+        }
+
+        void Application_Error ( object sender, EventArgs e )
+        {
+            //Unhandled error
+        }
+
+        void Session_Start ( object sender, EventArgs e )
+        {
+            Int32 count = ( Int32 ) Application [ SESSION_COUNT ];
+            count++;
+            Application [ SESSION_COUNT ] = count;
+
+
+            Response.Write ( "<hr />You are session #" + count );
+        }
+
+        void Session_End ( object sender, EventArgs e )
+        {
+            //This event only called when session state mode set to
+            //  InProc in the Web.config file. If session mode is set to
+            //  StateServer or SQLServer, the event is not raised
+            Int32 count = ( Int32 ) Application [ SESSION_COUNT ];
+            count--;
+            Application [ SESSION_COUNT ] = count;
         }
     }
 }
